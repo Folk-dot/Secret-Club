3054660ecc534b911ce27131ffb076fac402cd2e
Folk Krerkkiet <KrerkkietNJ@gmail.com>
2026-03-25 13:39:18 +0700

    feat/db-queries

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+require('dotenv').config();
+const path=require('node:path');
+
+const app=express();
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+const PORT=process.env.PORT
+app.listen(PORT,(err)=>{
+    if(err){
+        throw(err);
+    }
+    console.log(`app listening on port ${PORT}!`)
+})
